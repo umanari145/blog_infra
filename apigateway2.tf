943250970cb2d519681fc32cb3f92ea529768e67
umanari145 <umanari145@gmail.com>
2025-03-22 20:15:20 +0900

    loginのメソッドを実装

--- apigateway2.tf
+++ apigateway2.tf
@@ -29,6 +29,12 @@ resource "aws_api_gateway_resource" "res_4" {
   path_part   = "menus"
 }
 
+resource "aws_api_gateway_resource" "res_5" {
+  rest_api_id = aws_api_gateway_rest_api.blog_api2.id
+  parent_id   = aws_api_gateway_resource.res_1.id
+  path_part   = "login"
+}
+
 
 
 resource "aws_api_gateway_method" "method_1" {
@@ -416,6 +422,119 @@ resource "aws_api_gateway_integration_response" "method_5_integration_response" 
     aws_api_gateway_integration.method_5_integration
   ]
 }
+
+
+
+resource "aws_api_gateway_method" "method_6" {
+  rest_api_id   = aws_api_gateway_rest_api.blog_api2.id
+  resource_id   = aws_api_gateway_resource.res_5.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "method_6_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.blog_api2.id
+  resource_id             = aws_api_gateway_resource.res_5.id
+  http_method             = aws_api_gateway_method.method_6.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.blog_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "method_6_response" {
+  rest_api_id = aws_api_gateway_rest_api.blog_api2.id
+  resource_id = aws_api_gateway_resource.res_5.id
+  http_method = aws_api_gateway_method.method_6.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+
+resource "aws_api_gateway_integration_response" "method_6_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.blog_api2.id
+  resource_id = aws_api_gateway_resource.res_5.id
+  http_method = aws_api_gateway_method.method_6.http_method
+  status_code = "200"
+
+ response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+
+  depends_on = [
+    aws_api_gateway_resource.res_5,
+    aws_api_gateway_method.method_6,
+    aws_api_gateway_method_response.method_6_response,
+    aws_api_gateway_integration.method_6_integration
+  ]
+}
+resource "aws_api_gateway_method" "options_method_6" {
+  rest_api_id   = aws_api_gateway_rest_api.blog_api2.id
+  resource_id   = aws_api_gateway_resource.res_5.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options_method_6_integration" {
+  rest_api_id = aws_api_gateway_rest_api.blog_api2.id
+  resource_id = aws_api_gateway_resource.res_5.id
+  http_method = aws_api_gateway_method.options_method_6.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = <<EOF
+{
+  "statusCode": 200
+}
+EOF
+  }
+}
+
+resource "aws_api_gateway_method_response" "option_method_6_response" {
+  rest_api_id = aws_api_gateway_rest_api.blog_api2.id
+  resource_id = aws_api_gateway_resource.res_5.id
+  http_method = aws_api_gateway_method.options_method_6.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_method_6_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.blog_api2.id
+  resource_id = aws_api_gateway_resource.res_5.id
+  http_method = aws_api_gateway_method.options_method_6.http_method
+  status_code = aws_api_gateway_method_response.option_method_6_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+  }
+
+  depends_on = [
+    aws_api_gateway_resource.res_5,
+    aws_api_gateway_method.options_method_6,
+    aws_api_gateway_method_response.option_method_6_response,
+    aws_api_gateway_integration.options_method_6_integration
+  ]
+}
+
+
+
 
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on  = [aws_api_gateway_rest_api.blog_api2]
